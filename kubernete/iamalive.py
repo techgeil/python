31b7e2c7af52eccc3765ed3f02fdcb076ae8cf66
TechGeil <at114211@edu.aarhustech.dk>
2026-06-03 11:49:03 +0200

    push

--- kubernete/iamalive.py
+++ kubernete/iamalive.py
@@ -1,16 +1,18 @@
 import time
 import projekt as p
 i = 0
-
-time.sleep(5)
+#help(p.smile)
+#time.sleep(5)
 
 
 while True:
-    if i != 10:
-        print("Hej, jeg er i live! " + str(i), flush=True)
-    else:
+    if i == 10:
         print("Hej, jeg har vært i live i " + str(i) + " sekunder", flush=True)
-        p.smile()
+        p.smile(5)
+    else:
+        print("Hej, jeg er i live! " + str(i), flush=True)
+        
+        
     i += 1
     time.sleep(1)
     
