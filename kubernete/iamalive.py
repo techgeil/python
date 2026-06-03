39ad12d191f08374a79f73f1ee0cf7ab22c7d6f9
TechGeil <at114211@edu.aarhustech.dk>
2026-06-03 11:37:56 +0200

    projekt

--- kubernete/iamalive.py
+++ kubernete/iamalive.py
@@ -1,9 +1,17 @@
 import time
-
+import projekt as p
 i = 0
+
+time.sleep(5)
 
 
 while True:
-    print("Hej, jeg er i live! " + str(i), flush=True)
+    if i != 10:
+        print("Hej, jeg er i live! " + str(i), flush=True)
+    else:
+        print("Hej, jeg har vært i live i " + str(i) + " sekunder", flush=True)
+        p.smile()
     i += 1
     time.sleep(1)
+    
+        
---
+++ kubernete/projekt.py
@@ -0,0 +1,13 @@
+def smile(antal = 1):
+    """
+    print smileys til terminal den printer altid mindst en smiley
+    
+    Parameter:
+    
+    antal: int, antal smileys der skal printes
+    
+    Return: ingen
+    
+    """
+    for i in range(antal):
+        print(":)")
